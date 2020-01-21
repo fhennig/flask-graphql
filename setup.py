e8de7390d737df3e138771a8fe8dd68ebc600af5
Felix Hennig <mail@felixhennig.com>
2020-01-21 12:32:16 +0000

    Adapted dependencies to pull in graphql-core>=3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-required_packages = ["graphql-core>=2.1,<3", "flask>=0.7.0", "graphql-server-core>=1.1,<2"]
+required_packages = ["graphql-core>=3", "flask>=0.7.0", "graphql-server-core>=1.1,<2"]
 
 setup(
     name="Flask-GraphQL",
